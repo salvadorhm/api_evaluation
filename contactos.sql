eaa97998dab0ed118df2894d6496568f60ec7bc1
Salvador HM <salvadorhm@gmail.com>
2023-10-18 10:29:57 -0600

    SOLVED crear base de datos

--- contactos.sql
+++ contactos.sql
@@ -4,8 +4,8 @@ CREATE TABLE contactos (
     telefono TEXT
 );
 
-INSERT INTO contactos (email, nombre, telefonos)
+INSERT INTO contactos (email, nombre, telefono)
 VALUES ("juan@example.com", "Juan Pérez", "555-123-4567");
 
-INSERT INTO contactos (emial, nombre, telefono)
+INSERT INTO contactos (email, nombre, telefono)
 VALUES ("maria@example.com", "María García", "555-678-9012");
